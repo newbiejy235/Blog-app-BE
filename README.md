07320cff724b71f87049c833f806d2d18643fb09
newbiejy235 <dbankaii1@gmail.com>
2026-09-08 14:02:23 +0700

    add new progress

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Project ini berisi : 
 
+### Backend:
+
 1. ### setup 💻:
 - drizzle
 - mysql
@@ -11,9 +13,24 @@
 
 2. ### Penerapan cloudinary & Auth
 
+## ============================================================================
+
+### FrontEnd :
+
+1. ### setup 💻:
+- Next ts
+- shadcn ->  npx shadcn@latest init --preset b1FQhCDhI --template next
+
+
+## ============================================================================
+
 ## Bahasa pemrograman:
 - Typescript
 
 ## Framework:
 - Express
+- Next ts
+
+## Design System
+- shadcn ->  npx shadcn@latest init --preset b1FQhCDhI --template next
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,22 @@
 import express from "express";
-import authRotes from "./routes/auth/auth.route"
-import postRouter from "./routes/post/post.routes"
-import userRouter from "./routes/users/users.route"
+import authRotes from "./routes/auth/auth.route";
+import postRouter from "./routes/post/post.routes";
+import userRouter from "./routes/users/users.route";
+import cors from "cors";
 
-const app = express()
-const PORT = 5000
-app.use(express.json())
-app.use("/api/v1/auth",authRotes)
-app.use("/api/v1/post",postRouter)
-app.use("/api/v1/users",userRouter)
+const app = express();
+const PORT = 5000;
+app.use(cors());
+
+app.use(express.json());
+app.use("/api/v1/auth", authRotes);
+app.use("/api/v1/post", postRouter);
+app.use("/api/v1/users", userRouter);
 
 app.get("/", (req, res) => {
-    res.send("helllo")
-})
+  res.send("helllo");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running at  http://localhost:${PORT}/ see changes`);
-    
-})
+  console.log(`Server running at  http://localhost:${PORT}/ see changes`);
+});
