ac74f7d46f481c524ec0029e9b69e9d81e76cb44
newbiejy235 <dbankaii1@gmail.com>
2026-09-11 17:40:00 +0700

    add feat: menambahkan endpoint untuk ambil semua  data post

--- server/src/routes/post/post.routes.ts
+++ server/src/routes/post/post.routes.ts
@@ -7,6 +7,7 @@ import postController from "../../controllers/post/post.controller";
 const router = Router();
 // ambil semua data post dari user yang sudah login
 router.post("/", authenticate, uploadSingleImages, PostController.crreatePost);
+// ambil semua data post
 router.get("/", uploadSingleImages, PostController.getAll);
 
 router.get("/detail/:id", uploadSingleImages, PostController.detail);
