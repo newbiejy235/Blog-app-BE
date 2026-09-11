0ae119050f8825e23a5e6aa98ba4ab403dd0b6d9
newbiejy235 <dbankaii1@gmail.com>
2026-09-11 19:16:07 +0700

    add feat: menambahkan endpoint  delete favorite

--- server/src/routes/favorite/favorite.route.ts
+++ server/src/routes/favorite/favorite.route.ts
@@ -5,7 +5,11 @@ import { authenticate } from "../../middleware/auth.middleware";
 const router = Router()
 
 router.post("/", authenticate, favoriteController.postFavorite)
-
+router.delete(
+  "/:userId/post/:postId",
+  authenticate,
+  favoriteController.deleteFavorite
+);
 router.get("/:userId/post/:postId", authenticate, favoriteController.getFavorite)
 
 export default router
